6f589df18bdc6098fe3d1917158ed96335e3d108
Andre-Flores <andrefloresromero@gmail.com>
2023-08-11 14:10:01 -0600

    a ver main

--- Lab4P2_Eqipo9/src/lab4p2_eqipo9/Entrenador.java
+++ Lab4P2_Eqipo9/src/lab4p2_eqipo9/Entrenador.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package lab4p2_eqipo9;
 
-/**
- *
- * @author andre
- */
 public class Entrenador {
-    
+    private String nombre; 
+    private int edad;
+    private int dinero;
+  
 }
